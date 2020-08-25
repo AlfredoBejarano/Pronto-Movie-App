501b4a4c67e620ed4448f119d844c6f9e414b434
Alfredo Bejarano <alfredocbejarano@gmail.com>
2020-08-25 17:42:54 -0500

    Create MovieRepository.kt

---
+++ app/src/main/java/me/alfredobejarano/prontomovieapp/repository/MovieRepository.kt
@@ -0,0 +1,36 @@
+package me.alfredobejarano.prontomovieapp.repository
+
+import me.alfredobejarano.prontomovieapp.datasource.local.CacheManager
+import me.alfredobejarano.prontomovieapp.datasource.local.MovieDao
+import me.alfredobejarano.prontomovieapp.datasource.remote.TheMovideDbApiService
+import me.alfredobejarano.prontomovieapp.model.MovieListResultObjectToMovieMapper
+import me.alfredobejarano.prontomovieapp.model.local.Movie
+import javax.inject.Inject
+
+/**
+ * MovieRepository
+ */
+class MovieRepository @Inject constructor(
+    private val localDataSource: MovieDao,
+    private val cacheDataSource: CacheManager,
+    private val remoteDataSource: TheMovideDbApiService,
+    private val mapper: MovieListResultObjectToMovieMapper
+) {
+    private suspend fun getRemoteMovies() =
+        remoteDataSource.getMovies().results?.map(mapper::map) ?: emptyList()
+
+    private suspend fun getLocalMovies(includeAdultMovies: Boolean = false) =
+        localDataSource.readAll(includeAdultMovies)
+
+    private suspend fun cacheRemoteMovies(remoteResult: List<Movie>) {
+        remoteResult.forEach { movie -> localDataSource.createOrUpdate(movie) }
+        cacheDataSource.createMovieCache()
+    }
+
+    suspend fun getMovies(includeAdultMovies: Boolean = false) =
+        if (cacheDataSource.isMovieCacheValid()) {
+            getLocalMovies(includeAdultMovies)
+        } else {
+            getRemoteMovies().also { remoteResult -> cacheRemoteMovies(remoteResult) }
+        }
+}
